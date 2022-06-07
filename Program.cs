8598f0edc128df9d4d5e1b2dfa36b58491b745d4
fidel.antonio@hotmail.com <fidel.antonio@hotmail.com>
2022-06-07 00:22:04 -0400

    organizacion de los archivos segun su prioridad de funciones e incremento de ejercicosAgregar archivos de proyecto.2

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EjerciciosBAsicos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola mundo...");
+        }
+    }
+}
